64e7c9d9c84a5ce6f074c80087adb1cf64f52631
devops760 <60563189+devops760@users.noreply.github.com>
2020-07-26 11:13:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,24 +4,8 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-08f3d892de259504d"
   instance_type = "t2.micro"
 
   tags = {
